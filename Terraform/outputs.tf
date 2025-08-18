8f0ced66ae8168db6f495ad9e9ce410df2da2929
Wendell Segundo <xiamdev1@gmail.com>
2025-08-18 11:53:11 +0800

    fix:reference api

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,5 +1,5 @@
 output "api_invoke_url" {
   description = "Base URL for API Gateway"
-  value       = "${aws_apigatewayv2_api.http_api.api_endpoint}/prod"
+  value       = "${aws_apigatewayv2_api.serverless-app-api.api_endpoint}/prod"
 }
 
