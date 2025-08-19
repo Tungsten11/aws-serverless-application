1d849113482775fcaf6920f0ca8ee026af057acb
Wendell Segundo <xiamdev1@gmail.com>
2025-08-19 16:13:44 +0800

    add delete

--- lambda/app.py
+++ lambda/app.py
@@ -6,9 +6,10 @@ TABLE_NAME = os.environ.get("TABLE_NAME", "ItemsTable")
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table(TABLE_NAME)
 
+
 def lambda_handler(event, context):
     # For HTTP API v2
-    method = event.get("requestContext",{}).get("httpMethod")
+    method = event.get("requestContext", {}).get("httpMethod")
 
     if method == "POST":
         body = json.loads(event["body"])
@@ -24,6 +25,14 @@ def lambda_handler(event, context):
         return {
             "statusCode": 200,
             "body": json.dumps(response.get("Item", {}))
+        }
+
+    elif method == "DELETE":
+        body = json.loads(event["body"])
+        table.delete_item(Item={"id": body["id"], "value": body["value"]})
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"message": "Items Deleted!"})
         }
 
     return {
